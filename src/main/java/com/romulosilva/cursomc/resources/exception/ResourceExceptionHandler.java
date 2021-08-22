a044568329070d7ab0820e0d3f5e4b176c1dfe1e
RomulloSilva <romullo.dev@gmail.com>
2021-08-22 17:52:26 -0300

    Deletando uma Categoria com o DELETE

--- src/main/java/com/romulosilva/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/romulosilva/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.romulosilva.cursomc.resources.exception;
 
+import com.romulosilva.cursomc.services.exceptions.DataIntegrityException;
 import com.romulosilva.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +14,14 @@ public class ResourceExceptionHandler {
 
 
     @ExceptionHandler(ObjectNotFoundException.class)
-    public ResponseEntity<StandardError>ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+    public ResponseEntity<StandardError> ObjectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
         StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+
+    @ExceptionHandler(DataIntegrityException.class)
+    public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request) {
+        StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
--- src/main/java/com/romulosilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/romulosilva/cursomc/services/CategoriaService.java
@@ -2,8 +2,10 @@ package com.romulosilva.cursomc.services;
 
 import com.romulosilva.cursomc.domain.Categoria;
 import com.romulosilva.cursomc.repositories.CategoriaRepository;
+import com.romulosilva.cursomc.services.exceptions.DataIntegrityException;
 import com.romulosilva.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -31,5 +33,17 @@ public class CategoriaService {
 
         find(obj.getId());
         return repo.save(obj);
+    }
+
+    public void delete(Integer id) {
+
+        find(id);
+        try {
+            repo.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+
+            throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+        }
+
     }
 }
