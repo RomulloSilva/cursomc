18cc8d65b7503ea054f884c282e913ccd9c441c4
RomulloSilva <romullo.dev@gmail.com>
2021-08-22 17:28:09 -0300

    Inserindo uma nova Categoria com o post

--- src/main/java/com/romulosilva/cursomc/services/CategoriaService.java
+++ src/main/java/com/romulosilva/cursomc/services/CategoriaService.java
@@ -1,13 +1,12 @@
 package com.romulosilva.cursomc.services;
 
-import java.util.Optional;
-
+import com.romulosilva.cursomc.domain.Categoria;
+import com.romulosilva.cursomc.repositories.CategoriaRepository;
 import com.romulosilva.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.romulosilva.cursomc.domain.Categoria;
-import com.romulosilva.cursomc.repositories.CategoriaRepository;
+import java.util.Optional;
 
 
 @Service
@@ -21,5 +20,10 @@ public class CategoriaService {
         Optional<Categoria> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException(
                 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+    }
+
+    public Categoria insert(Categoria obj) {
+        obj.setId(null);
+        return repo.save(obj);
     }
 }
