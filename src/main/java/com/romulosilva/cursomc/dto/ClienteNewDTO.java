f013feaa13928237ff1019696dbcd772c60fe1bf
RomulloSilva <romullo.dev@gmail.com>
2021-09-12 18:01:11 -0300

    Validação costumizada de CPF e CNPJ

--- src/main/java/com/romulosilva/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/romulosilva/cursomc/dto/ClienteNewDTO.java
@@ -1,23 +1,48 @@
 package com.romulosilva.cursomc.dto;
 
+import com.romulosilva.cursomc.services.validation.ClienteInsert;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
+@ClienteInsert
 public class ClienteNewDTO implements Serializable {
 
     private static final long serialVersionUID = -5742634987120253332L;
+
+    @NotEmpty(message = "Campo nome é de preenchimento obrigatório.")
+    @Length(min = 5, max = 120, message = "O tamanho deve ser entre 5 e 120 caracteres.")
     private String nome;
+
+    @NotEmpty(message = "Campo e-mail é de preenchimento obrigatório.")
+    @Email(message = "E-mail inválido.")
     private String email;
+
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String cpfOuCnpj;
+
     private Integer tipo;
 
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String logradouro;
+
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String numero;
+
     private String complemento;
+
     private String bairro;
+
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String cep;
 
+    @NotEmpty(message = "Preenchimento obrigatório.")
     private String telefone1;
+
     private String telefone2;
+
     private String telefone3;
 
     private Integer cidadeId;
